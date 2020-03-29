5ee7d2e222b2496334796bf5222b79c81e6fc964
AdministratorHY <626456774@qq.com>
2020-03-29 11:44:48 +0800

    test git

---
+++ test/src/test/Temp.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.util.Arrays;
+
+public class Temp {
+	public static void main(String[] args) {
+		test1();
+	}
+	
+	private static void test1(){
+		double rs = 10 / (double)3;
+		System.out.println(rs);
+		System.out.println(Math.ceil(rs));
+	}
+	
+	private static void test0(){
+		String str = "a-b-c";
+		String[] ss = str.split("-");
+		System.out.println(Arrays.toString(ss));
+	}
+}
